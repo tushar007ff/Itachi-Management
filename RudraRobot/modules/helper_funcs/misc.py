1a3ae94d408b6aa05f6ebba58ae024c27fc37a92
RUDRA JAAT <136905244+RUDRA-JAAT@users.noreply.github.com>
2024-01-11 07:30:52 +0530

    Update misc.py

--- RudraRobot/modules/helper_funcs/misc.py
+++ RudraRobot/modules/helper_funcs/misc.py
@@ -91,7 +91,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
                     "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
                 ),
                 EqInlineKeyboardButton(
-                    "• ʙᴀᴄᴋ •", callback_data="Mainback"
+                    "• ʙᴀᴄᴋ •", callback_data="rudraback"
                 ),
                 EqInlineKeyboardButton(
                     "▷", callback_data="{}_next({})".format(prefix, modulo_page)
