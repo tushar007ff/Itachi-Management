bcc29fc23a99a65d888c57cce88666540f729ef0
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-02-12 05:53:33 +0530

    Update __main__.py

--- RudraRobot/__main__.py
+++ RudraRobot/__main__.py
@@ -200,13 +200,13 @@ def start(update: Update, context: CallbackContext):
 
             x=update.effective_message.reply_sticker(
                 "CAACAgUAAxkBAAI33mLYLNLilbRI-sKAAob0P7koTEJNAAIOBAACl42QVKnra4sdzC_uKQQ")
-            time.sleep(0.9)
+            time.sleep(0.3)
             x.delete() 
             usr = update.effective_user
             lol = update.effective_message.reply_text(
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
             )
-            time.sleep(0.9)
+            time.sleep(0.3)
             lol.edit_text("❤")
             time.sleep(0.2)
             lol.edit_text("⚡")
